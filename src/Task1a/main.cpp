2092ee16b4efc7251ebdce6ab3add66233247ba8
Oleksii Doronin <oleksii.doronin@tuni.fi>
2021-10-20 17:53:39 +0300

    task1a base code

---
+++ src/Task1a/main.cpp
@@ -0,0 +1,39 @@
+#include "Application.h"
+
+#include <iostream>
+
+
+const Application::VectorCString validationLayers = {
+    "VK_LAYER_KHRONOS_validation",
+};
+
+const Application::VectorCString deviceExtensions = {
+    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+};
+
+
+
+int main()
+{
+    Application app(
+        800, // width
+        600, // height
+        PROJECT_NAME, // App name.
+        std::string(PROJECT_NAME) + "/shader.vert.spv", // vert shader path
+        std::string(PROJECT_NAME) + "/shader.frag.spv", // frag shader path
+        validationLayers,
+        deviceExtensions
+        );
+
+    try
+    {
+        app.run();
+    }
+    catch ( const std::exception& e )
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
