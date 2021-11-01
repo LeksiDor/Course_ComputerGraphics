951f3edda1ac8b7e252b4228a05797b73b93a40c
Oleksii Doronin <oleksii.doronin@gmail.com>
2021-11-01 04:24:46 +0200

    updated Task 1a

--- src/Task1a/main.cpp
+++ src/Task1a/main.cpp
@@ -1,39 +1,75 @@
-#include "Application.h"
+#include "ApplicationBase.h"
 
+#include <glm/glm.hpp>
 #include <iostream>
 
 
-const Application::VectorCString validationLayers = {
-    "VK_LAYER_KHRONOS_validation",
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
 };
 
-const Application::VectorCString deviceExtensions = {
-    VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+
+const std::vector<Vertex> vertices = {
+    { {  0.0f, -0.5f }, { 1.0f, 0.0f, 0.0f} },
+    { {  0.5f,  0.5f }, { 0.0f, 1.0f, 0.0f} },
+    { { -0.5f,  0.5f }, { 0.0f, 0.0f, 1.0f} },
 };
 
+const std::vector<uint32_t> indices = { 0, 2, 1 };
+
+
+
+class AppExample : public svk::ApplicationBase
+{
+public:
+
+    virtual VkVertexInputBindingDescription getVertexBindingDescription() const override
+    {
+        return { 0, sizeof(Vertex), VK_VERTEX_INPUT_RATE_VERTEX };
+    }
+
+    virtual std::vector<VkVertexInputAttributeDescription> getVertexAttributeDescriptions() const override
+    {
+        return {
+            { 0, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, pos) },
+            { 1, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, color) },
+        };
+    }
+
+    virtual void InitSwapChain() override
+    {
+        swapchain->Init(
+            commandPool,
+            this,
+            vertices,
+            indices,
+            std::string(PROJECT_NAME) + "/shader.vert.spv",
+            std::string(PROJECT_NAME) + "/shader.frag.spv"
+        );
+    }
+};
 
 
 int main()
 {
-    Application app(
-        800, // width
-        600, // height
-        PROJECT_NAME, // App name.
-        std::string(PROJECT_NAME) + "/shader.vert.spv", // vert shader path
-        std::string(PROJECT_NAME) + "/shader.frag.spv", // frag shader path
-        validationLayers,
-        deviceExtensions
-        );
-
+    AppExample app;
     try
     {
-        app.RunMainLoop();
+        app.Init(
+            800, // width
+            600, // height
+            PROJECT_NAME, // App name.
+            { "VK_LAYER_KHRONOS_validation" },
+            { VK_KHR_SWAPCHAIN_EXTENSION_NAME }
+        );
+        app.MainLoop();
+        app.Destroy();
     }
     catch ( const std::exception& e )
     {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
 }
